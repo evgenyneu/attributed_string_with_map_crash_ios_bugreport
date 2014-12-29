84d3bea011033ec5d779848e0d3836fa2d24e447
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-12-29 13:36:42 +1100

    Initial Commit

---
+++ attributed_strgin_with_map_crash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  attributed_strgin_with_map_crash
+//
+//  Created by Evgenii Neumerzhitckii on 29/12/2014.
+//  Copyright (c) 2014 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
